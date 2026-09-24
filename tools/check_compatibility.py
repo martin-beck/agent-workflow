a2abfcbf623dd665f3da0e59fbfa514cb2260165
martin-beck <martin.beck2@gmx.de>
2026-09-24 11:47:20 +0200

    chore(umbrella): pin runtime v0.1.1
    Signed-off-by: martin-beck <martin.beck2@gmx.de>

--- tools/check_compatibility.py
+++ tools/check_compatibility.py
@@ -51,8 +51,8 @@ def check() -> None:
             "8ec460ec99baf8061d1644a2532b5b9beb47b75b",
         ),
         "agent-workflow-runtime": (
-            "v0.1.0",
-            "c3be277bc996a7470a8c22ee5846259ca030beca",
+            "v0.1.1",
+            "ed1911bd9c6530ad508aef596c36bfc4ffdfcde9",
         ),
     }
     for repository, (release, commit) in expected.items():
